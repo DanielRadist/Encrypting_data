803872fb332c6150096f972cf942396bb8184ff2
Рахманин Данила <daniel.radist@gmail.com>
2022-04-11 20:45:36 +0700

    Добавьте файлы проекта.

---
+++ Encrypting_data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Encrypting_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
