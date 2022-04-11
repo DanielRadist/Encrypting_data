03d91aaf0444a005549b28af4c2c6351ae18f48a
Рахманин Данила <daniel.radist@gmail.com>
2022-04-11 21:06:11 +0700

    Add Encrypting class and empty methods

---
+++ Encrypting_data/Encrypting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypting_data
+{
+    internal static class Encrypting
+    {
+        // Symmetric:
+        public static byte[] SymmetricEncryption(string data, byte[] key)
+        {
+            return null;
+        }
+
+        public static string SymmetricDecryption(byte[] data, byte[] key)
+        {
+            return null;
+        }
+
+        // Asymmetric:
+        public static byte[] AsymmetricEncryption(string data, byte[] key)
+        {
+            return null;
+        }
+
+        public static string AsymmetricDecryption(byte[] data, byte[] key)
+        {
+            return null;
+        }
+    }
+}
